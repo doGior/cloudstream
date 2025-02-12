39a3dc9826970ee3974af628e7132e315331922f
doGior <43815981+doGior@users.noreply.github.com>
2025-02-12 02:59:20 +0100

    Focused row gets bigger (no animation) in main page

--- app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeChildItemAdapter.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeChildItemAdapter.kt
@@ -1,8 +1,14 @@
 package com.lagradost.cloudstream3.ui.home
 
+import android.animation.ValueAnimator
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.cardview.widget.CardView
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.viewbinding.ViewBinding
 import com.lagradost.cloudstream3.R
 import com.lagradost.cloudstream3.SearchResponse
@@ -17,6 +23,7 @@ import com.lagradost.cloudstream3.ui.settings.Globals.TV
 import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
 import com.lagradost.cloudstream3.utils.UIHelper.isBottomLayout
 import com.lagradost.cloudstream3.utils.UIHelper.toPx
+
 
 class HomeScrollViewHolderState(view: ViewBinding) : ViewHolderState<Boolean>(view) {
     /*private fun recursive(view : View) : Boolean {
@@ -35,7 +42,7 @@ class HomeScrollViewHolderState(view: ViewBinding) : ViewHolderState<Boolean>(vi
         if (state) {
             wasFocused = false
             // only refocus if tv
-            if(isLayout(TV)) {
+            if (isLayout(TV)) {
                 itemView.requestFocus()
             }
         }
@@ -43,23 +50,25 @@ class HomeScrollViewHolderState(view: ViewBinding) : ViewHolderState<Boolean>(vi
 }
 
 class HomeChildItemAdapter(
-    fragment: Fragment,
+    private val fragment: Fragment,
     id: Int,
     private val nextFocusUp: Int? = null,
     private val nextFocusDown: Int? = null,
     private val clickCallback: (SearchClickCallback) -> Unit,
-) :
-    BaseAdapter<SearchResponse, Boolean>(fragment, id) {
+) : BaseAdapter<SearchResponse, Boolean>(fragment, id) {
+
     var isHorizontal: Boolean = false
     var hasNext: Boolean = false
 
+    private val _isRowFocused = MutableLiveData(false)
+    private val isRowFocused: LiveData<Boolean> get() = _isRowFocused
+
+    companion object {
+        private val recyclerViewFocusMap = mutableMapOf<Int, Boolean>()
+    }
+
     override fun onCreateContent(parent: ViewGroup): ViewHolderState<Boolean> {
         val expanded = parent.context.isBottomLayout()
-        /* val layout = if (bottom) R.layout.home_result_grid_expanded else R.layout.home_result_grid
-
-         val root = LayoutInflater.from(parent.context).inflate(layout, parent, false)
-         val binding = HomeResultGridBinding.bind(root)*/
-
         val inflater = LayoutInflater.from(parent.context)
         val binding = if (expanded) HomeResultGridExpandedBinding.inflate(
             inflater,
@@ -79,43 +88,57 @@ class HomeChildItemAdapter(
                 binding.backgroundCard.apply {
                     val min = 114.toPx
                     val max = 180.toPx
-
                     layoutParams =
                         layoutParams.apply {
-                            width = if (!isHorizontal) {
-                                min
-                            } else {
-                                max
-                            }
-                            height = if (!isHorizontal) {
-                                max
-                            } else {
-                                min
-                            }
+                            width = if (!isHorizontal) min else max
+                            height = if (!isHorizontal) max else min
                         }
                 }
             }
 
             is HomeResultGridExpandedBinding -> {
-                binding.backgroundCard.apply {
-                    val min = 114.toPx
-                    val max = 180.toPx
-
-                    layoutParams =
-                        layoutParams.apply {
-                            width = if (!isHorizontal) {
-                                min
-                            } else {
-                                max
-                            }
-                            height = if (!isHorizontal) {
-                                max
-                            } else {
-                                min
-                            }
-                        }
+                val minSize = 114
+                val maxSize = 180
+                val (baseWidth, baseHeight) = if (isHorizontal) {
+                    maxSize to minSize
+                } else {
+                    minSize to maxSize
                 }
+                val normalWidth = baseWidth.toPx
+                val normalHeight = baseHeight.toPx
+                val focusedWidth = (baseWidth * 1.4).toInt().toPx
+                val focusedHeight = (baseHeight * 1.4).toInt().toPx
 
+                isRowFocused.observe(fragment.viewLifecycleOwner) {
+                    val isRecyclerViewFocused = recyclerViewFocusMap[id] ?: false
+
+                    if (isRecyclerViewFocused) {
+//                            animateCardSize(
+//                                binding.backgroundCard,
+//                                normalWidth,
+//                                normalHeight,
+//                                focusedWidth,
+//                                focusedHeight
+//                            )
+                        binding.backgroundCard.updateLayoutParams {
+                            width = focusedWidth
+                            height = focusedHeight
+                        }
+                    } else {
+                        binding.backgroundCard.updateLayoutParams {
+                            width = normalWidth
+                            height = normalHeight
+                        }
+//                        animateCardSize(
+//                            binding.backgroundCard,
+//                            focusedWidth,
+//                            focusedHeight,
+//                            normalWidth,
+//                            normalHeight
+//                        )
+
+                    }
+                }
                 if (position == 0) { // to fix tv
                     binding.backgroundCard.nextFocusLeftId = R.id.nav_rail_view
                 }
@@ -138,5 +161,46 @@ class HomeChildItemAdapter(
         )
 
         holder.itemView.tag = position
+        holder.itemView.setOnFocusChangeListener { _, gotFocus ->
+            if (gotFocus) {
+                recyclerViewFocusMap[id] = true // Mark this RecyclerView as focused
+            } else {
+                recyclerViewFocusMap[id] = false // Update focus state
+            }
+            _isRowFocused.value = recyclerViewFocusMap[id]
+        }
+    }
+
+    private fun animateCardSize(
+        cardView: CardView,
+        startWidth: Int,
+        startHeight: Int,
+        endWidth: Int,
+        endHeight: Int
+    ) {
+        val widthAnimator = ValueAnimator.ofInt(startWidth, endWidth)
+        val heightAnimator = ValueAnimator.ofInt(startHeight, endHeight)
+
+        widthAnimator.addUpdateListener { animation ->
+            val newWidth = animation.animatedValue as Int
+            val layoutParams = cardView.layoutParams
+            layoutParams.width = newWidth
+            cardView.layoutParams = layoutParams
+        }
+
+        heightAnimator.addUpdateListener { animation ->
+            val newHeight = animation.animatedValue as Int
+            val layoutParams = cardView.layoutParams
+            layoutParams.height = newHeight
+            cardView.layoutParams = layoutParams
+        }
+
+        widthAnimator.duration = 300
+        heightAnimator.duration = 300
+        widthAnimator.interpolator = DecelerateInterpolator()
+        heightAnimator.interpolator = DecelerateInterpolator()
+
+        widthAnimator.start()
+        heightAnimator.start()
     }
 }
